8fdcb30ac72dcf61fe1eaf64fd2f1dfcd9d3d60e
matheus.amaral <matheus.amaral@aglets.com.br>
2024-03-09 18:15:52 -0300

    feat: creating home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,26 @@
-import wallpaper from "../../assets/wallpaper.jpg";
-import wallpaper2 from "../../assets/wallpaper2.avif";
 import { GoArrowUpRight } from "react-icons/go";
 import { Carousel as ResponsiveCarousel  } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
+import wallpaper from "../../assets/wallpaper.jpg";
+import wallpaper2 from "../../assets/wallpaper2.avif";
+import img1 from "../../assets/img1.webp"
+
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
+
 const Home = () => {
     return (
         <main>
-            <div className=" flex flex-col gap-2 mb-10 text-white">
-                <ResponsiveCarousel className="h-screen w-full mb-5" autoPlay infiniteLoop showArrows={false} showThumbs={false} showStatus={false}>
+            <section className="flex flex-col gap-2 text-white">
+                <ResponsiveCarousel className="h-screen w-full" autoPlay infiniteLoop showArrows={false} showThumbs={false} showStatus={false}>
                     <div>
                         <img src={wallpaper2} className="h-screen xl:h-full" style={{ maxHeight: '800px'}} alt="carousel2" />
                     </div>
@@ -16,8 +28,7 @@ const Home = () => {
                         <img src={wallpaper} className="h-screen xl:h-full" style={{ maxHeight: '800px'}} alt="carousel1" />
                     </div>
                 </ResponsiveCarousel>
-
-                {/* <img src={wallpaper} alt="wallpaper" /> */}
+                
                 <div className="flex flex-col gap-8 justify-center items-center mt-32 2xl:mt-46 md:w-5/6 lg:w-4/6 xl:w-3/6 3xl:w-2/6 mx-auto select-none absolute top-40 right-0 left-0 max-w-xl:">
                     <h1 className="text-4xl px-10 md:text-5xl font-bold text-center" style={{textShadow: "10px 10px 10px rgba(0, 0, 0, 5)"}}>INSTALAÇÃO ELÉTRICA INDUSTRIAL</h1>
                     <p className="text-lg text-center w-10/12" style={{textShadow: "10px 10px 10px rgba(0, 0, 0, 5)"}}>Supere a complexidade com precisão: confie em nossa expertise em projetos industriais de alta complexidade.</p>
@@ -27,7 +38,55 @@ const Home = () => {
                         <GoArrowUpRight fontSize={20}/>
                     </button>
                 </div>
-            </div>
+            </section>
+
+            <section className="bg-gray-400">
+                <div className="flex flex-col mx-10 sm:flex-row justify-center gap-20 xl:mx-auto max-w-6xl py-20">
+                    <h1 className="text-2xl w-full">Por que contratar uma empresa <strong>especialista</strong> em instalações elétricas?</h1>
+
+                    <div className="flex flex-col gap-2 items-start">
+                        <span>
+                            Com a Sartori, você economiza e preserva o seu patrimônio. Entre os motivos para contratar uma empresa especialista está a garantia de possuir uma instalação elétrica segura, ininterrupta e sem oscilações.
+                        </span>
+
+                        <button className="flex items-center gap-2 bg-transparent text-black rounded-sm mt-2 text-sm hover:bg-gray-300 p-2 transition-all">
+                            CONHEÇA MAIS MOTIVOS E SURPREENDA-SE 
+                            <GoArrowUpRight fontSize={20}/>
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section className="grid grid-cols-2 w-full max-h-96">
+                <div className="flex flex-col gap-6 p-10 w-full max-w-6xl h-full py-20 ml-10">
+                    <h1 className="text-4xl font-bold">PROJETOS ELÉTRICOS</h1>
+                    <p className="text-md">A análise dos projetos elétricos da Sartori é realizada com o objetivo de atender às necessidades individuais de cada cliente. É essencial compreender se o cliente requer um projeto totalmente novo ou uma modificação em um projeto existente.</p>
+                    <button className="flex items-center justify-center gap-2 bg-gray-900 text-white p-5 rounded-sm mt-2 max-w-72 ">
+                        VEJA TODOS OS PROJETOS
+                        <GoArrowUpRight fontSize={20}/>
+                    </button>
+                </div>
+
+                <Carousel>
+                    <CarouselContent className="relative">
+                        <CarouselItem>
+                            <img src={img1} className="h-96"/>
+                        </CarouselItem>
+                        
+                        <CarouselItem>
+                            <img src={img1} className="h-96"/>
+                        </CarouselItem>
+
+                        <CarouselItem>
+                            <img src={img1} className="h-96"/>
+                        </CarouselItem>
+                    </CarouselContent>
+
+                    <CarouselPrevious className="absolute left-0" />
+                    <CarouselNext className="absolute right-10" />
+                </Carousel>
+
+            </section>
         </main>
     )
 }
